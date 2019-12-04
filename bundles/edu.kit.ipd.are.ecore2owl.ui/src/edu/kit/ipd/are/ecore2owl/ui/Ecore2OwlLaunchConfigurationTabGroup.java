bd648bd2c850c3cb721fe36d31aaf4a39ed30a73
Jan Keim <jan.keim@kit.edu>
2019-12-04 16:03:28 +0100

    small adjustments

--- bundles/edu.kit.ipd.are.ecore2owl.ui/src/edu/kit/ipd/are/ecore2owl/ui/Ecore2OwlLaunchConfigurationTabGroup.java
+++ bundles/edu.kit.ipd.are.ecore2owl.ui/src/edu/kit/ipd/are/ecore2owl/ui/Ecore2OwlLaunchConfigurationTabGroup.java
@@ -6,6 +6,13 @@ import org.eclipse.debug.ui.ILaunchConfigurationTab;
 
 public class Ecore2OwlLaunchConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
 
+    /**
+     * Standard constructor
+     */
+    public Ecore2OwlLaunchConfigurationTabGroup() {
+        super();
+    }
+
     @Override
     public void createTabs(ILaunchConfigurationDialog arg0, String arg1) {
         setTabs(new ILaunchConfigurationTab[] { new Ecore2OwlLaunchConfigurationTab() });
